a7ccef97171e728f249f5df47c3d90effb5a41e8
omarraf <omarrafiqq@gmail.com>
2025-12-03 10:48:13 -0800

    Add SQL queries and schema for users, feeds, feed follows, and posts

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -2,3 +2,15 @@
 INSERT INTO users (id, created_at, updated_at, name)
 VALUES ($1, $2, $3, $4)
 RETURNING *;
+
+-- TODO: Add a query to get a user by their API key
+
+-- name: GetUserByAPIKey :one
+SELECT * FROM users WHERE api_key = $1;
+
+-- name: GetUserByID :one
+SELECT * FROM users WHERE id = $1;
+
+
+
+
